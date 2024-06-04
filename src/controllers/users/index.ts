f1dce6383104f4bb48b6f309e496227474c26da8
charlserose555 <charlserose4060@gmail.com>
2024-06-04 22:47:11 +0200

    * complete the affiliate system

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -77,8 +77,6 @@ export const signup = async (req: Request, res: Response) => {
     try {
         const user = req.body;
 
-        console.log("user", user);
-
         const ip = getIPAddress(req);
         // const ipCount = await Users.countDocuments({ ip: { '$regex': ip.ip, '$options': 'i' } })
         // if (ipCount > 1) {
@@ -116,15 +114,14 @@ export const signup = async (req: Request, res: Response) => {
 
         let u_result = await new Users(newuser).save();
 
-        console.log(u_result);
-
         await new Bonus({email: newuser.email}).save();
 
         if(u_result.rReferral != 0) {
             const userLevel_1 = await Users.findOne({'iReferral' : newuser.rReferral});
-            console.log("userLevel_1.email", userLevel_1.email)
 
             await Bonus.findOneAndUpdate({email : userLevel_1.email}, { $addToSet: { 'level1_users': newuser.email }})
+
+            await Users.findOneAndUpdate({email: userLevel_1.email}, {$inc: {'invite_members' : 1}})
 
             const level_1_Code = userLevel_1.rReferral;
             let level_2_email = "";
@@ -149,8 +146,6 @@ export const signup = async (req: Request, res: Response) => {
 };
 
 export const getVIPLevelInfo =  async (req: Request, res: Response) => {
-    console.log("ASDfasdfsadf")
-
     const paymentInfo = await PaymentSetting.find();
 
     return res.json({success: true, data: paymentInfo});
@@ -178,8 +173,6 @@ export const getBankCardInfo =  async (req: Request, res: Response) => {
     const {data} = req.body;
 
     const bankCards = await BankCard.find({email : data.email});
-
-    console.log(bankCards)
 
     return res.json({success: true, data: bankCards});
 }
@@ -214,19 +207,22 @@ export const getDepositOrderInfo =  async (req: Request, res: Response) => {
     if(order) {
         return res.json({success: true, data: order});
     } else {
-        return res.json({success: false, data: "failure on get deposit order"});
+        return res.json({success: false, data: "Failure on get deposit order"});
     }
 }
 
 export const confirmDepositOrderInfo =  async (req: Request, res: Response) => {
     const {data} = req.body;
     
-    const order = await DepositOrder.findOneAndUpdate({order_id: data.order_id, status: "pending"}, {ref_no: data.ref_no});
     
+    const order = await DepositOrder.findOneAndUpdate({order_id: data.order_id, status: "order"}, {ref_no: data.ref_no, status: "pending"});
+    
+    console.log(data)
+
     if(order) {
         return res.json({success: true, data: order});
     } else {
-        return res.json({success: false, data: "failure on get deposit order"});
+        return res.json({success: false, msg: "Failure on confirm deposit order"});
     }
 }
 
@@ -241,8 +237,6 @@ export const orderWithDrawal =  async (req: Request, res: Response) => {
 
     const bankInfo = await BankCard.findOne({accountNumber : data.bankAccount, _id : data.bankAccountId});
     if(bankInfo) {
-        console.log("bankInfo", bankInfo)
-
         await new WithDrawalOrder({
             email : data.email,
             amount : data.amount,
@@ -279,8 +273,6 @@ export const getBonusInfo =  async (req: Request, res: Response) => {
         }
     ])
     
-    console.log("bonus", bonus)
-
     if(bonus) {
         return res.json({success: true, data: bonus});
     } else {
@@ -296,8 +288,6 @@ export const applyAllBonus =  async (req: Request, res: Response) => {
     if(!userInfo || !userInfo.verified) {
         return res.json({success: false, msg: "Please deposit on our site for verify"});
     }
-
-    console.log("data", data);
 
     const bonus = await Bonus.aggregate([
         {
@@ -366,6 +356,430 @@ export const getBonusHistory =  async (req: Request, res: Response) => {
     } else {
         return res.json({success: false, msg: ""});
     }
+}
+
+export const getFriendInfo =  async (req: Request, res: Response) => {
+    const {data} = req.body;
+    
+    const perPage = 10;
+
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'level1_users',
+          foreignField: 'email',
+          as: 'user_details',
+                pipeline: [
+            {
+              $project: {
+                _id: 1,
+                balance: 1,
+                invite_members: 1,
+                email: 1
+              }
+            }
+          ]
+        }
+      },
+      {
+        $unwind: '$user_details'
+      },
+      {
+        $project: {
+          _id: 1,
+          email: 1,
+          amount: 1,
+          user_details: 1,
+        }
+      },
+      {
+        $skip: (data.page - 1) * perPage
+      },
+      {
+        $limit: perPage
+      }
+    ])
+
+    if(result) {
+        return res.json({success: true, msg: "",  data: result});
+    } else {
+        return res.json({success: false, msg: ""});
+    }
+}
+
+export const getStatisticByUser = async (req: Request, res: Response) => {
+    const {data} = req.body;
+    
+    let promises = [];
+
+    promises.push(getLevel1Users_RegitesterationInfo(data),
+                    getLevel2Users_RegitesterationInfo(data),
+                    getLevel1Users_firstDepositInfo(data),
+                    getLevel2Users_firstDepositInfo(data),
+                    getLevel1Users_DepositInfo(data),
+                    getLevel2Users_DepositInfo(data),
+                    getLevel1Users_withDrawalInfo(data),
+                    getLevel2Users_withDrawalInfo(data),
+                    getTodayBetInfo(data),
+                    getBetInfo(data));
+
+    let results = await Promise.all(promises);
+
+    if(results) {
+        return res.json({success: true, msg: "",  data: results});
+    } else {
+        return res.json({success: false, msg: "Failure on get team info"});
+    }
+
+}
+
+const getLevel1Users_RegitesterationInfo = async (data: any) => {
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'level1_users',
+                foreignField: 'email',
+                as: 'user_1_details',
+            }
+        },
+        {
+            $unwind: '$user_1_details'
+        },
+        {
+          $match: { 
+            "user_1_details.createdAt": {$gt: new Date(data.date)}
+          }
+        }, 
+        {
+            $project: {
+            _id: 1,
+            email: 1,
+            amount: 1,
+            user_1_details: 1,
+            }
+        },
+        {
+            $group: { _id: "$email",
+            count: { $sum: 1 },
+            sum: { $sum: "$user_1_details.balance" }
+         }}
+    ]);
+
+    return result[0];
+}
+
+const getLevel2Users_RegitesterationInfo = async (data: any) => {
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+        {
+             $lookup: {
+                from: 'users',
+                localField: 'level2_users',
+                foreignField: 'email',
+                as: 'user_2_details'
+            }
+        },
+        {
+            $unwind: '$user_2_details'
+        },
+        {
+          $match: { 
+            "user_2_details.createdAt": {$gt: new Date(data.date)}
+          }
+        }, 
+        {
+            $project: {
+            _id: 1,
+            email: 1,
+            amount: 1,
+            user_2_details: 1,
+            }
+        },
+        {
+            $group: { _id: "user_2_details.email",
+            count: { $sum: 1 },
+            sum: { $sum: "$user_2_details.balance" }
+         }}
+    ]);
+
+    return result[0];
+}
+
+const getLevel1Users_firstDepositInfo = async (data: any) => {
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+        {
+            $lookup: {
+              from: 'deposit_orders',
+              localField: 'level1_users',
+              foreignField: 'email',
+              as: 'user_1_details'
+            }
+          },
+            {
+            $unwind: '$user_1_details'
+          },
+            {
+                $match: {
+                    'user_1_details.status': 'pending'
+                },
+            },
+            {
+                $group: {
+                _id: "user_1_details.email",
+                firstItem: { $first: "$user_1_details.createdAt" }
+             }
+            },
+            {
+                $match: {
+                    'firstItem': {$gt: new Date(data.date)}
+                },
+            },
+            {
+                $group: {
+                _id: "user_1_details.email",
+                count: { $sum: 1 }
+             }
+        },
+    ]);
+
+    return result[0];
+}
+
+const getLevel2Users_firstDepositInfo = async (data: any) => {
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+        {
+            $lookup: {
+              from: 'deposit_orders',
+              localField: 'level2_users',
+              foreignField: 'email',
+              as: 'user_2_details'
+            }
+          },
+            {
+            $unwind: '$user_2_details'
+          },
+            {
+                $match: {
+                    'user_2_details.status': 'pending'
+                },
+            },
+            {
+                $group: {
+                _id: "user_2_details.email",
+                firstItem: { $first: "$user_2_details.createdAt" }
+             }
+            },
+            {
+                $match: {
+                    'firstItem': {$gt: new Date(data.date)}
+                },
+            },
+            {
+                $group: {
+                _id: "user_2_details.email",
+                count: { $sum: 1 }
+             }
+        },
+    ]);
+
+    return result[0];
+}
+
+const getLevel1Users_DepositInfo = async (data: any) => {
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+        {
+            $lookup: {
+                from: 'deposit_orders',
+                localField: 'level1_users',
+                foreignField: 'email',
+                as: 'user_1_details',
+            }
+        },
+        {
+            $unwind: '$user_1_details'
+        },
+        {
+            $match: {
+                'user_1_details.status': 'completed'
+            },
+        },
+        { $group: { _id: "user_1_details.email", count: { $sum: 1 } , sum: { $sum: "$user_1_details.amount" }} }
+    ]);
+
+    return result[0];
+}
+
+const getLevel2Users_DepositInfo = async (data: any) => {
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+        {
+            $lookup: {
+                from: 'deposit_orders',
+                localField: 'level2_users',
+                foreignField: 'email',
+                as: 'user_2_details',
+            }
+        },
+        {
+            $unwind: '$user_2_details'
+        },
+        {
+            $match: {
+                'user_2_details.status': 'completed'
+            },
+        },
+        { $group: { _id: "user_2_details.email", count: { $sum: 1 } , sum: { $sum: "$user_2_details.amount" }} }
+    ]);
+
+    return result[0];
+}
+
+const getLevel1Users_withDrawalInfo = async (data: any) => {
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+        {
+            $lookup: {
+                from: 'withdrawal_orders',
+                localField: 'level1_users',
+                foreignField: 'email',
+                as: 'user_1_details',
+            }
+        },
+        {
+            $unwind: '$user_1_details'
+        },
+        {
+            $match: {
+                'user_1_details.status': 'completed'
+            },
+        },
+        { $group: { _id: "user_1_details.email", count: { $sum: 1 } , sum: { $sum: "$user_1_details.amount" }} }
+    ]);
+
+    return result[0];
+}
+
+const getLevel2Users_withDrawalInfo = async (data: any) => {
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+        {
+            $lookup: {
+                from: 'withdrawal_orders',
+                localField: 'level2_users',
+                foreignField: 'email',
+                as: 'user_2_details',
+            }
+        },
+        {
+            $unwind: '$user_2_details'
+        },
+        {
+            $match: {
+                'user_2_details.status': 'completed'
+            },
+        },
+        { $group: { _id: "user_2_details.email", count: { $sum: 1 } , sum: { $sum: "$user_2_details.amount" }} }
+    ]);
+
+    return result[0];
+}
+
+const getBetInfo = async (data: any) => {
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+        {
+            $lookup: {
+              from: 'wingo_bettings',
+              localField: 'level1_users',
+              foreignField: 'email',
+              as: 'user_1_details'
+            }
+          },
+            {
+            $unwind: '$user_1_details'
+          },
+        { $group: { _id: {email: "$user_1_details.email"}, count: { $sum: 1 }, amount: { $sum: "$user_1_details.amount" } } },
+        { $group: { _id: {email: "_id"}, count: { $sum: 1 }, amount: { $sum: "$amount" } } }
+    ]);
+
+    return result[0];
+}
+
+const getTodayBetInfo = async (data: any) => {
+    const result = await Bonus.aggregate([
+        {
+            $match: {
+                email: data.email
+            },
+        },
+        {
+            $lookup: {
+              from: 'wingo_bettings',
+              localField: 'level1_users',
+              foreignField: 'email',
+              as: 'user_1_details'
+            }
+          },
+            {
+            $unwind: '$user_1_details'
+          },
+          {
+            $match: { 
+              "user_1_details.createdAt": {$gt: new Date(data.date)}
+            }
+          }, 
+        { $group: { _id: {email: "$user_1_details.email"}, count: { $sum: 1 }, amount: { $sum: "$user_1_details.amount" } } },
+        { $group: { _id: {email: "_id"}, count: { $sum: 1 }, amount: { $sum: "$amount" } } }
+    ]);
+
+    return result[0];
 }
 
 export const verifyCode = async (req: Request, res: Response) => {
